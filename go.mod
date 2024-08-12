cfc16b68a92f5d4546da9c20a97048c95257036e
Pedro Mendes <108732350+Pedro-Mendes-IMGArena@users.noreply.github.com>
2024-08-12 07:04:42 +0000

    adding metrics endpoint

--- go.mod
+++ go.mod
@@ -4,8 +4,10 @@ go 1.22.5
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/bytedance/sonic v1.12.0 // indirect
 	github.com/bytedance/sonic/loader v0.2.0 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.5 // indirect
@@ -26,8 +28,14 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/prometheus/client_golang v1.19.1 // indirect
+	github.com/prometheus/client_model v0.5.0 // indirect
+	github.com/prometheus/common v0.48.0 // indirect
+	github.com/prometheus/procfs v0.12.0 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
+	github.com/zsais/go-gin-prometheus v0.1.0 // indirect
 	golang.org/x/arch v0.8.0 // indirect
 	golang.org/x/crypto v0.25.0 // indirect
 	golang.org/x/net v0.27.0 // indirect
--- routes/routes.go
+++ routes/routes.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/getsentry/sentry-go"
 	"github.com/gin-gonic/gin"
+	ginprometheus "github.com/zsais/go-gin-prometheus"
 )
 
 func ErrorHandler() gin.HandlerFunc {
@@ -27,26 +28,30 @@ func ErrorHandler() gin.HandlerFunc {
 func RegisterRoutes(server *gin.Engine) {
 	server.Use(ErrorHandler())
 
-	//MIDDLEWARE Setup
-	checkEventIdParam := middleware.ValidateParam("eventId", "int64")
-
-	authenticatedRoute := server.Group("/")
-	authenticatedRoute.Use(middleware.Authenticate)
-
-	authenticatedRouteWithEventId := server.Group("/")
-	authenticatedRouteWithEventId.Use(middleware.Authenticate, checkEventIdParam, middleware.LoadEventById)
-
-	routeWithEventId := server.Group("/")
-	routeWithEventId.Use(checkEventIdParam, middleware.LoadEventById)
+	p := ginprometheus.NewPrometheus("gin")
+	p.Use(server)
 
 	server.POST("/signup", createUser)
 	server.POST("/login", login)
 
 	server.GET("/events", getEvents)
 
+	//MIDDLEWARE Setup
+	checkEventIdParam := middleware.ValidateParam("eventId", "int64")
+
+	routeWithEventId := server.Group("/")
+	routeWithEventId.Use(checkEventIdParam, middleware.LoadEventById)
+
 	routeWithEventId.GET("events/:eventId", getEvent)
 
+	authenticatedRoute := server.Group("/")
+	authenticatedRoute.Use(middleware.Authenticate)
+
 	authenticatedRoute.POST("/events", createEvent)
+
+	authenticatedRouteWithEventId := authenticatedRoute.Group("/")
+	authenticatedRouteWithEventId.Use(checkEventIdParam, middleware.LoadEventById)
+
 	authenticatedRouteWithEventId.PUT("/events/:eventId", middleware.IsEventOwner, updateEvent)
 	authenticatedRouteWithEventId.DELETE("/events/:eventId", middleware.IsEventOwner, deleteEvent)
 	authenticatedRouteWithEventId.POST("/events/:eventId/register", registerUserToEvent)
